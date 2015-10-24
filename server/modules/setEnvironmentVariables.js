894c41add8bf13ef8f694515229bb1e985a3b2b3
Joe Burdick <josephdburdick@gmail.com>
2015-10-24 13:40:24 -0400

    Add environment variable handling in startup.
    Add condition to avoid Meteor settings when they don't exist.

---
+++ server/modules/setEnvironmentVariables.js
@@ -0,0 +1,7 @@
+let setEnvironmentVariables = () => {
+  if (Meteor.settings.private){
+      Meteor.settings.private.MAIL_URL = process.env.MAIL_URL;
+  }
+};
+
+Modules.server.setEnvironmentVariables = setEnvironmentVariables;
