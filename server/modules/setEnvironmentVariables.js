ffa30155328aa02cad2253cfb1f44cb4afc56d36
Joe Burdick <josephdburdick@gmail.com>
2015-10-24 14:09:37 -0400

    Add condition to avoid Meteor settings when they don't exist.

--- server/modules/setEnvironmentVariables.js
+++ server/modules/setEnvironmentVariables.js
@@ -1,5 +1,7 @@
 let setEnvironmentVariables = () => {
-  Meteor.settings.private.MAIL_URL = process.env.MAIL_URL;
+  if (Meteor.settings.private){
+      Meteor.settings.private.MAIL_URL = process.env.MAIL_URL;
+  }
 };
 
 Modules.server.setEnvironmentVariables = setEnvironmentVariables;
