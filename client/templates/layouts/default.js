05be89080e4847fb87ec8b62f6b5d6a47cdf4c4d
Tom Van Schoor <tom@tutuka.com>
2015-12-22 21:03:34 +0200

    Make the indenting whitespace consistent.
    Why:
    
    * Consistency is one of the most important things in good code.

--- client/templates/layouts/default.js
+++ client/templates/layouts/default.js
@@ -3,20 +3,17 @@ const authenticated = () => {
 }
 
 const handleRedirect = () => {
-	let currentRoute = FlowRouter.getRouteName();
+  let currentRoute = FlowRouter.getRouteName();
   let authData = FlowRouter.current().route.group.options.authentication;
-	if ( authData && authData.requireLoggedIn ^ authenticated() ) {
-		FlowRouter.go( authData.redirectPath );
-		return true;
+  if ( authData && authData.requireLoggedIn ^ authenticated() ) {
+    FlowRouter.go( authData.redirectPath );
+    return true;
   }
 };
 
 Template.default.helpers({
   loggingIn() {
     return Meteor.loggingIn();
-  },
-  authenticated() {
-    return authenticated();
   },
   redirectRequired() {
     return handleRedirect();
