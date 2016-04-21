aae5ce9228eff9326c330b6980708cde7aae88f8
tmcdeveloper <developer@themeteorchef.com>
2016-04-21 11:00:37 -0500

    wire up custom loader for documents list

--- imports/ui/containers/documents-list.js
+++ imports/ui/containers/documents-list.js
@@ -1,13 +1,16 @@
 import { composeWithTracker } from 'react-komposer';
 import { Documents } from '../../api/documents/documents.js';
 import { DocumentsList } from '../components/documents-list.js';
+import { Loading } from '../components/loading.js';
 
 const composer = (params, onReady) => {
   const subscription = Meteor.subscribe('documents');
   if (subscription.ready()) {
     const documents = Documents.find().fetch();
-    onReady(null, { documents });
+    // setTimeout is used to visually buffer the loading spinner so it's not
+    // jarring. Can just call onReady directly if you wish :)
+    setTimeout(() => { onReady(null, { documents }); }, 500);
   }
 };
 
-export default composeWithTracker(composer)(DocumentsList);
+export default composeWithTracker(composer, Loading)(DocumentsList);
