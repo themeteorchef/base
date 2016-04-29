9131938628e89a83f77935d8830070fc6b7725c6
Devan Beitel <devan.beitel@gmail.com>
2016-04-28 22:07:41 -0500

    Changed onReady to onData to align with Komposer documentation.

--- imports/ui/containers/documents-list.js
+++ imports/ui/containers/documents-list.js
@@ -3,11 +3,11 @@ import { Documents } from '../../api/documents/documents.js';
 import { DocumentsList } from '../components/documents-list.js';
 import { Loading } from '../components/loading.js';
 
-const composer = (params, onReady) => {
+const composer = (params, onData) => {
   const subscription = Meteor.subscribe('documents');
   if (subscription.ready()) {
     const documents = Documents.find().fetch();
-    onReady(null, { documents });
+    onData(null, { documents });
   }
 };
 
