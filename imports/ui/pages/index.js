9f77e06056916aab6ec108fdba085aa2db434ac6
tmcdeveloper <developer@themeteorchef.com>
2016-07-27 20:20:00 -0500

    bump Meteor and Base version in README.md and index.js

--- imports/ui/pages/index.js
+++ imports/ui/pages/index.js
@@ -6,6 +6,6 @@ export const Index = () => (
     <h2>Base</h2>
     <p>A starting point for Meteor applications.</p>
     <p><a className="btn btn-success" href="https://themeteorchef.com/base" role="button">Read the Documentation</a></p>
-    <p style={ { fontSize: '16px', color: '#aaa' } }>Currently at v4.5.0</p>
+    <p style={ { fontSize: '16px', color: '#aaa' } }>Currently at v4.6.0</p>
   </Jumbotron>
 );
