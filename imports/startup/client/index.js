3daf322e92d9d71e9059cd12103edb6b9701c858
themeteorchef <business@themeteorchef.com>
2016-08-15 20:19:56 -0500

    bump dependencies and patch a few things
    - Bring Meteor up to 1.4.0.1.
    - Bring Meteor packages up to date.
    - Bring NPM packages up to date.
    - Fix console warnings from #157. Closes #157. Closes #162.
    - Remove import of bootstrap.js (was redundant). Closes #170.
    
    Closes #172

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,6 +1,5 @@
 import { Bert } from 'meteor/themeteorchef:bert';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
 import './routes.js';
 
 Bert.defaults.style = 'growl-top-right';
