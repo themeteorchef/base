7ece709c23568f60bb0a505c447024f6de36e436
James Scaggs <james.scaggs@gmail.com>
2016-09-19 15:08:14 -0500

    initial commit

---
+++ imports/collections/test-data.js
@@ -0,0 +1,5 @@
+import { Mongo } from 'meteor/mongo';
+import MongoImport from 'mongoimport';
+
+export const TestData = new Mongo.Collection('test-data');
+
--- imports/ui/pages/index.js
+++ imports/ui/pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Jumbotron } from 'react-bootstrap';
+import SearchBar from '../components/search-bar';
 
 export const Index = () => (
-  <Jumbotron className="text-center">
-    <h2>Base</h2>
-    <p>A starting point for Meteor applications.</p>
-    <p><a className="btn btn-success" href="https://themeteorchef.com/base" role="button">Read the Documentation</a></p>
-    <p style={ { fontSize: '16px', color: '#aaa' } }>Currently at v4.7.0</p>
-  </Jumbotron>
+  <div>
+  	<Jumbotron className="text-center">
+  	  <h3>Search local powersports deals</h3>
+  	<SearchBar />
+  	</Jumbotron>
+  </div>
 );
