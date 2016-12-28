cc427569dfc313ba9584c93fb4e7a03c90714ee1
Greg Stephens <greg@udon.org>
2016-12-28 15:06:31 -0800

    Griddle Listview

--- imports/ui/components/DocumentsList.js
+++ imports/ui/components/DocumentsList.js
@@ -1,11 +1,41 @@
 import React from 'react';
-import { ListGroup, ListGroupItem, Alert } from 'react-bootstrap';
+import { ListGroup, Alert, ButtonGroup, Button } from 'react-bootstrap';
+import Griddle from 'griddle-react';
+
+var actionButtons = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <ButtonGroup>
+                    <Button bsSize="small" href={'/documents/' + this.props.rowData._id}>View</Button>
+                    <Button bsSize="small" href={'/documents/' + this.props.rowData._id + '/edit'}>Edit</Button>
+                </ButtonGroup>
+            </div>
+        );
+    }
+});
+
+const columnList = ['title','body','actions'];
+const columnMetadata = [
+  {columnName: 'title', order: "1", displayName: 'Title', cssClassName: 'col-xs-3' },
+  {columnName: 'body', order: "2", displayName: 'Body', cssClassName: 'col-xs-6' },
+  {columnName: 'actions', order: "3", customComponent: actionButtons, sortable: false, cssClassName: 'col-xs-3',
+    displayName: 'Actions'}
+];
 
 const DocumentsList = ({ documents }) => (
   documents.length > 0 ? <ListGroup className="DocumentsList">
-    {documents.map(({ _id, title }) => (
-      <ListGroupItem key={ _id } href={`/documents/${_id}`}>{ title }</ListGroupItem>
-    ))}
+    <Griddle
+        results={documents}
+        useGriddleStyles={false}
+        tableClassName={'griddle-flex table table-bordered table-striped table-hover'}
+        settingsToggleClassName='btn btn-default'
+        useCustomPagerComponent={true}
+        showFilter={true}
+        showSettings={true}
+        resultsPerPage="20"
+        columnMetadata={columnMetadata}
+        columns={columnList}/>
   </ListGroup> :
   <Alert bsStyle="warning">No documents yet.</Alert>
 );
