d608ac71952c6bf097068269215ba5e187b8e4e7
themeteorchef <business@themeteorchef.com>
2017-01-13 12:13:33 -0600

    adjust spacing to match eslint rules

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -2,15 +2,12 @@ import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
 import { Accounts } from 'meteor/accounts-base';
 
-/* Create a default admin user but *only in non-production environments!*
- * This is to avoid a base app being deployed in production with a known generic admin user
- */
-if ( !Meteor.isProduction ) {
+if (!Meteor.isProduction) {
   const users = [{
     email: 'admin@admin.com',
     password: 'password',
     profile: {
-      name: {first: 'Carl', last: 'Winslow'},
+      name: { first: 'Carl', last: 'Winslow' },
     },
     roles: ['admin'],
   }];
