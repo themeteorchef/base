237c9a371d04ef64053115fd6e5462fe17812709
michelfloyd <michel@floyd.com>
2017-02-24 15:16:56 -0800

    Return NotFound template when a document is not found instead of a blank page w/errors.

--- imports/ui/pages/EditDocument.js
+++ imports/ui/pages/EditDocument.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import DocumentEditor from '../components/DocumentEditor.js';
+import DocumentEditor from '../components/DocumentEditor';
+import NotFound from './NotFound';
 
-const EditDocument = ({ doc }) => (
-  <div className="EditDocument">
-    <h4 className="page-header">Editing "{ doc.title }"</h4>
-    <DocumentEditor doc={ doc } />
-  </div>
-);
+const EditDocument = ({ doc }) => {
+  if (!doc) return <NotFound />;
+  return (
+    <div className="EditDocument">
+      <h4 className="page-header">Editing "{ doc.title }"</h4>
+      <DocumentEditor doc={ doc }/>
+    </div>
+  )
+};
 
 EditDocument.propTypes = {
   doc: React.PropTypes.object,
--- imports/ui/pages/ViewDocument.js
+++ imports/ui/pages/ViewDocument.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { ButtonToolbar, ButtonGroup, Button } from 'react-bootstrap';
 import { browserHistory } from 'react-router';
 import { Bert } from 'meteor/themeteorchef:bert';
-import { removeDocument } from '../../api/documents/methods.js';
+import { removeDocument } from '../../api/documents/methods';
+import NotFound from './NotFound';
 
 const handleEdit = (_id) => {
   browserHistory.push(`/documents/${_id}/edit`);
-}
+};
 
 const handleRemove = (_id) => {
   if (confirm('Are you sure? This is permanent!')) {
@@ -21,20 +22,23 @@ const handleRemove = (_id) => {
   }
 };
 
-const ViewDocument = ({ doc }) => (
-  <div className="ViewDocument">
-    <div className="page-header clearfix">
-      <h4 className="pull-left">{ doc && doc.title }</h4>
-      <ButtonToolbar className="pull-right">
-        <ButtonGroup bsSize="small">
-          <Button onClick={ () => handleEdit(doc._id) }>Edit</Button>
-          <Button onClick={ () => handleRemove(doc._id) } className="text-danger">Delete</Button>
-        </ButtonGroup>
-      </ButtonToolbar>
+const ViewDocument = ({ doc }) => {
+  if (!doc) return <NotFound />;
+  return (
+    <div className="ViewDocument">
+      <div className="page-header clearfix">
+        <h4 className="pull-left">{ doc && doc.title }</h4>
+        <ButtonToolbar className="pull-right">
+          <ButtonGroup bsSize="small">
+            <Button onClick={ () => handleEdit(doc._id) }>Edit</Button>
+            <Button onClick={ () => handleRemove(doc._id) } className="text-danger">Delete</Button>
+          </ButtonGroup>
+        </ButtonToolbar>
+      </div>
+      { doc && doc.body }
     </div>
-    { doc && doc.body }
-  </div>
-);
+  )
+};
 
 ViewDocument.propTypes = {
   doc: React.PropTypes.object,
