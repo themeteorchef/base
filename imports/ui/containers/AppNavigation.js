972bd67cfb1046b001835a5fa404991407e83c1f
GreatFallz <toine.vankampen@gmail.com>
2017-04-15 08:37:08 +1000

    Update to subscribe to top level field instead

--- imports/ui/containers/AppNavigation.js
+++ imports/ui/containers/AppNavigation.js
@@ -1,7 +1,13 @@
 import { composeWithTracker } from 'react-komposer';
 import { Meteor } from 'meteor/meteor';
 import AppNavigation from '../components/AppNavigation.js';
+import Loading from '../components/Loading.js';
 
-const composer = (props, onData) => onData(null, { hasUser: Meteor.user() });
+const composer = (params, onData) => {
+  const subscription = Meteor.subscribe('users.info');
+  if (subscription.ready()) {
+    onData(null, { hasUser: Meteor.user() });
+  }
+};
 
-export default composeWithTracker(composer, {}, {}, { pure: false })(AppNavigation);
+export default composeWithTracker(composer, Loading, {}, { pure: false })(AppNavigation);
