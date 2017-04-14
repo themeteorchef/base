e46d66a49136595b9e8b8630d33a0097303f3df9
GreatFallz <toine.vankampen@gmail.com>
2017-04-15 08:33:19 +1000

    Update publications.js
    Add this.UserId context.

--- imports/api/users/server/publications.js
+++ imports/api/users/server/publications.js
@@ -1,7 +1,13 @@
 import { Meteor } from 'meteor/meteor';
 
-Meteor.publish('users.info', () => Meteor.users.find({}, {
-  fields: {
-    name: 1,
-  },
-}));
+Meteor.publish('users.info', function userInfoPublish() {
+  if (!this.userId) {
+    return this.ready();
+  }
+
+  return Meteor.users.find({}, {
+    fields: {
+      name: 1,
+    },
+  });
+});
